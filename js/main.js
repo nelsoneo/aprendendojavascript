f21748740729c39f073d6e36c3bd853b4ec3be5b
Nelson Hernandez Guerra <ng19239@migrate.local>
2019-12-30 11:31:20 -0300

    subindo primer codigo javascript

---
+++ js/main.js
@@ -0,0 +1,118 @@
+function botao(){
+    document.getElementById("feliz2020").innerHTML = "nelson feliz";
+    //console.log(document.getElementById("feliz2020"));
+}
+/*
+function redireccionar(){
+//window.open("https://www.google.com/");
+window.location.href = "https://www.google.com/";
+}
+*/
+function trocar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Migrate";
+   elemento.innerHTML =  "Migrate";
+  //  alert("ótimo");
+}
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaochange(elemento){
+console.log(elemento.value);
+}
+
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Migrate feliz 2020";
+   elemento.innerHTML =  "Migrate feliz 2020";
+}
+
+/*
+function botao(){
+    alert("obrigado");
+}
+
+/*
+function validaidade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+return validar;
+}
+
+var idade = prompt("qual é sua idade");
+console.log(validaidade(idade));
+
+/*
+function soma(n1, n2){
+    return n1 +n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5,13));
+alert(setReplace("vai Nelson", "Nelson", "Migrate"));
+
+
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    console.log(count)
+}
+
+/*
+var count = 2;
+while(count <= 5){
+console.log(count);
+count ++;
+//count = count + 1;
+}
+
+/*
+var idade = prompt("Qual é sua idade")
+
+if (idade >= 18){
+    alert("maior de idade")
+}else{
+    alert("Menor de idade")
+}
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome)
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+/*var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+//alert(lista[1]);*/
+
+/*var nome = "Nelson Hernandez";
+var idade = 33;
+var idade2 = 10;
+var frase = "cuba o melhor do mundo"
+alert(nome+" Tem "+idade+" anos");
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade * idade2);
+console.log(frase.toLowerCase());*/
